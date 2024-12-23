45c7c3e730580afbd6e23808c38a175588f97114
alish <alifshania222@gmail.com>
2024-12-23 20:20:41 +0330

    remove nima from gruop

--- src/App.js
+++ src/App.js
@@ -8,10 +8,9 @@ export const App = () => {
   return (
     <div className='dark:bg-blue-950 bg-rose-200 h-max text-center text-white'>
       <div className='pt-10'>
-        <h1 className='text-6xl font-bold'> A&N Groups</h1>
+        <h1 className='text-6xl font-bold'>ScriptToori</h1>
       </div>
       <Profile />
-      <Profile2 />
     </div>
   )
 }
