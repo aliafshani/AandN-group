0d4454bf421036509044b43074856317db50e856
alish <alifshania222@gmail.com>
2025-01-17 06:02:07 +0330

    add nima?

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Profile2 from './Components/Profile2'
 
 export const App = () => {
   return (
-    <div className='dark:bg-purple-950 bg-rose-200 h-max text-center text-white'>
+    <div className='dark:bg-purple-950  bg-rose-200 h-max text-center text-white'>
       <div className='pt-10'>
         <h1 className='text-6xl font-bold'>ScriptToori</h1>
         <h2 className='text-4xl mt-10 font-bold'>coming soon ...</h2>
